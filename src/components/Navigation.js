406f137816aee057515faf81f539434596acdbed
Shivam Nagpal <shivamnagpal432@gmail.com>
2020-08-19 20:04:39 +0530

    Center the heading

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Navigation extends React.Component{
     render(){        
         return(
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark text-light">
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark text-light header">
                 <h3 className="d-block">RECIPE SEARCH</h3>
             </nav>
         )
